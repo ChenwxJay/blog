bc157f0f6d509f2203c38bf468c123a27c6eb975
DH-PC\Administrator <aspwebchh@hotmail.com>
2018-01-21 23:55:17 +0800

    分成查找功能

--- common/lexeme.go
+++ common/lexeme.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"errors"
 	"regexp"
+	"encoding/json"
 )
 
 func interface2Error(data interface{}) error {
@@ -45,7 +46,6 @@ func map2HttpParams(data map[string]string) string {
 		result  = append(result, item)
 	}
 	var str = strings.Join(result,"&")
-	fmt.Println(str)
 	return str
 }
 
@@ -80,6 +80,41 @@ func HttpPost(url string, data map[string]string) (result string, resultErr erro
 }
 
 const LEXEME_SYS_KEY  =   "chhblog"
+const LEXEME_URL_CREATE = "http://chhblog.com:8000/create"
+const LEXEME_URL_FIND = "http://chhblog.com:8000/find"
+
+type LexemeFindResult struct {
+	DataList []int
+	DataCount int
+}
+
+type LemexeServiceResult struct {
+	Code int
+	Message string
+	Data LexemeFindResult
+}
+
+func json2lexemeServiceResult( jsonString string )  *LemexeServiceResult {
+	var newJson = strings.Replace(jsonString,"code","Code",-1)
+	newJson = strings.Replace(newJson,"message","Message",-1)
+	newJson = strings.Replace(newJson,"data","Data",-1)
+
+	var regex = regexp.MustCompile(`\[(\[[^\]]*]),(\d+)]`)
+	var replaceResult = regex.ReplaceAllStringFunc(newJson, func( match string ) string {
+		var group = regex.FindStringSubmatch(match)
+		var dataList = group[1]
+		var dataCount = group[2]
+		var converted = "{\"DataList\":"+ dataList +",\"DataCount\":"+ dataCount +"}"
+		return converted
+	})
+	var result = new( LemexeServiceResult )
+	var err = json.Unmarshal([]byte(replaceResult),result)
+	if err != nil {
+		return nil
+	} else {
+		return result
+	}
+}
 
 func LexemeCreate(dataId int, _type int, text string) bool {
 	var data = make(map[string]string)
@@ -87,6 +122,31 @@ func LexemeCreate(dataId int, _type int, text string) bool {
 	data["data_id"] = strconv.Itoa( dataId )
 	data["type"] = strconv.Itoa( _type )
 	data["sys_key"] = LEXEME_SYS_KEY
-	HttpPost("https://www.chhblog.com", data)
+	var jsonResult,err = HttpPost(LEXEME_URL_CREATE, data)
+	if err != nil {
+		return false
+	}
+	var serviceResult = json2lexemeServiceResult(jsonResult)
+	if serviceResult == nil {
+		return false
+	}
+	if serviceResult.Code != 0 {
+		return false
+	}
 	return true;
+}
+
+func LexemeFind(text string, _type int, pageIndex int, pageSize int) (dataCount int, dataList []int)  {
+	var data = make(map[string]string)
+	data["text"] = text
+	data["page_index"] = strconv.Itoa( pageIndex )
+	data["page_size"] = strconv.Itoa( pageSize )
+	data["type"] = strconv.Itoa( _type )
+	data["sys_key"] = LEXEME_SYS_KEY
+	var jsonResult,err = HttpPost(LEXEME_URL_FIND, data)
+	if err != nil {
+		return 0,make([]int,0)
+	}
+	var serviceResult = json2lexemeServiceResult(jsonResult)
+	return serviceResult.Data.DataCount, serviceResult.Data.DataList
 }
